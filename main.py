6e3afba96e9e0f94256583aa0e97a313042c6f7d
Campbell Padgett <campbellpadgett5@gmail.com>
2022-03-05 12:09:21 -0600

    setup

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    print('Chicago, MET, or RJK')
+    source = input('Which source will we convert? ')
+    
+    if source == 'Chicago':
+        csv_file = ''
+    elif source == 'MET':
+        csv_file = ''
+    elif source =='RJK':
+        csv_file = ''
+
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
