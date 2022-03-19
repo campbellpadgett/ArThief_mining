4d491d17ce2270f21e2a5a4a8eb7046c36a972a0
Campbell Padgett <campbellpadgett5@gmail.com>
2022-03-19 15:39:54 -0500

    adding csv writer functionality

--- met.py
+++ met.py
@@ -1,8 +1,10 @@
-from cgi import test
 import csv
+import time
+from datetime import datetime
 from typing import NoReturn
-from utils import csv_traverse
-from requests import get
+from utils import csv_traverse, create_new_csv
+import asyncio
+
 
 
 
@@ -12,28 +14,32 @@ def met() -> NoReturn:
     source = 'MET'
     original_file = '../MET/MetObjects.csv'
     key_object_type_terms = ['Paintings', 'Paintings-Decorative']
+    desired_met_data = {
+        'title': 9,
+        'artist': 18,
+        'artist_nationality': 22,
+        'artist_display_bio': 19,
+        'culture': 10,
+        'era': 11,
+        'gender': 25,
+        'nation': 38,
+        'medium': 31,
+        'source': 50,
+        'date_of_release': 28,
+        'image_link': 4
+    }
 
     artworks = csv_traverse(original_file, key_object_type_terms, source)
-
     
-
-    # test_artwork = artworks[0]
-
-    test_artwork = ['2009.224', 'False', 'True', 'True', '35155', '374', 'Arms and Armor', '2009', 'Painting', 'Guidobaldo II della Rovere, Duke of Urbino (1514–1574), With his  Armor by Filippo Negroli', 'Italian', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'ca. 1580–85', '1555', '1610', 'Oil on copper', '5 1/2 x 4 in. (14 x 10.2 cm); frame: 7 1/8 x 4 7/8 in. (18.1 x 12.5 cm)', 'Purchase, Arthur Ochs Sulzberger Gift, 2009', '', '', '', '',
-                    '', '', '', '', '', '', '', 'Paintings', '', 'http://www.metmuseum.org/art/collection/search/35155', 'https://www.wikidata.org/wiki/Q56042443', '', 'Metropolitan Museum of Art, New York, NY', 'Armor|Men|Portraits', 'http://vocab.getty.edu/page/aat/300226591|http://vocab.getty.edu/page/aat/300025928|http://vocab.getty.edu/page/aat/300015637', 'https://www.wikidata.org/wiki/Q20793164|https://www.wikidata.org/wiki/Q8441|https://www.wikidata.org/wiki/Q134307']
+    print('--------------------------------------')
+    print(len(artworks[:26]))
     
+    start = time.time()
+    asyncio.run(create_new_csv(filename='met.csv', rows=artworks, desired_data=desired_met_data))
+    end = time.time()
 
-    # for idx in range(len(test_artwork)):
-    #     print(test_artwork[idx], idx)
-
-    object_id = test_artwork[4]
-    res = get(f'https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}')
-
-    primaryImage, primaryImageSmall = res.json()['primaryImage'], res.json()['primaryImageSmall']
-
-    print(primaryImage, primaryImageSmall)
-
-    
+    print('--------------------------------------')
+    print(f'Wrote {len(artworks[:26])} new rows in {end - start} seconds')
 
 
 
--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ def csv_traverse(csv_file: str, key_terms: List[str], source: str) -> List:
     or non-Photo related artworks, returns results"""
 
     traversed_csv = []
+    start = time.time()
     with open(csv_file) as csv_file_opened:
 
         csv_reader = csv.reader(csv_file_opened, delimiter=',')
@@ -29,6 +30,8 @@ def csv_traverse(csv_file: str, key_terms: List[str], source: str) -> List:
             
             traversed_csv.append(row)
         
+        end = time.time()
+        print(f'traversed {len(traversed_csv)} rows in {end - start} seconds')
         return traversed_csv
         # print(traversed_csv[:5])
         # print(type(traversed_csv))
@@ -54,7 +57,7 @@ def get_row_indicies(row: List[str]) -> List[str]:
 
 
 
-async def process_csv_entry(row: str, session: aiohttp.ClientSession, count: int, desired_data: Dict[str, int]):
+async def process_csv_entry(row: str, session: aiohttp.ClientSession, count: int, desired_data: Dict[str, int], writer: csv.writer):
     """Takes a row from a csv file and makes a get request with the url stored 
     in it. Then stores new url and desired data in new csv row"""
 
@@ -76,12 +79,10 @@ async def process_csv_entry(row: str, session: aiohttp.ClientSession, count: int
         print(f'Storing {primaryImage}, {primaryImageSmall}...')
         new_line.append(primaryImage)
         new_line.append(primaryImageSmall)
-        print(new_line)
+        writer.writerow(new_line)
 
 
-
-
-async def create_new_csv(rows: list[str]):
+async def create_new_csv(filename: str, rows: list[str], desired_data: Dict[str, int]):
     """Takes rows of csv data and persists them to new csv using the process_csv_entry function
     and the event loop from asyncio and aiohttp"""
 
@@ -91,14 +92,19 @@ async def create_new_csv(rows: list[str]):
     ssl_context = ssl.create_default_context(cafile=certifi.where())
     conn = aiohttp.TCPConnector(ssl=ssl_context, limit=10)
 
-    async with aiohttp.ClientSession(connector=conn, headers=headers) as session:
-        tasks = []
-        count = 0
-        for row in rows:
-            task = asyncio.ensure_future(process_csv_entry(
-                row=row, session=session, count=count, desired_data=desired_met_data))
-            tasks.append(task)
-        await asyncio.gather(*tasks, return_exceptions=True)
+    with open(filename, mode='w') as test_file:
+        met_writer = csv.writer(test_file, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        met_writer.writerow(['Title', 'Artist'])
+
+        async with aiohttp.ClientSession(connector=conn, headers=headers) as session:
+            tasks = []
+            count = 0
+            for row in rows:
+                task = asyncio.ensure_future(process_csv_entry(
+                    row=row, session=session, count=count, desired_data=desired_data, writer=met_writer))
+                tasks.append(task)
+            await asyncio.gather(*tasks, return_exceptions=True)
 
 
 
@@ -113,11 +119,9 @@ if __name__ == '__main__':
     # key_terms = ['Paintings', 'Paintings-Decorative']
     # csv_traverse('../MET/MetObjects.csv', key_terms, 'MET')
 
-    test_csv_row = ['2009.224', 'False', 'True', 'True', '35155', '374', 'Arms and Armor', '2009', 'Painting', 'Guidobaldo II della Rovere, Duke of Urbino (1514–1574), With his  Armor by Filippo Negroli', 'Italian', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 'ca. 1580–85', '1555', '1610', 'Oil on copper', '5 1/2 x 4 in. (14 x 10.2 cm); frame: 7 1/8 x 4 7/8 in. (18.1 x 12.5 cm)', 'Purchase, Arthur Ochs Sulzberger Gift, 2009', '', '', '', '',
-                '', '', '', '', '', '', '', 'Paintings', '', 'http://www.metmuseum.org/art/collection/search/35155', 'https://www.wikidata.org/wiki/Q56042443', '', 'Metropolitan Museum of Art, New York, NY', 'Armor|Men|Portraits', 'http://vocab.getty.edu/page/aat/300226591|http://vocab.getty.edu/page/aat/300025928|http://vocab.getty.edu/page/aat/300015637', 'https://www.wikidata.org/wiki/Q20793164|https://www.wikidata.org/wiki/Q8441|https://www.wikidata.org/wiki/Q134307']
-    csv_rows = [test_csv_row]
-    print(len(csv_rows))
-    print('--------------------------------------')
+    source = 'MET'
+    original_file = '../MET/MetObjects.csv'
+    key_object_type_terms = ['Paintings', 'Paintings-Decorative']
     desired_met_data = {
         'title': 9,
         'artist': 18,
@@ -132,7 +136,12 @@ if __name__ == '__main__':
         'date_of_release': 28,
         'image_link': 4
     }
+    
+    csv_rows = csv_traverse(original_file, key_object_type_terms, source)
+
+    print(len(csv_rows[:26]))
+    print('--------------------------------------')
     start = time.time()
-    asyncio.run(create_new_csv(csv_rows))
+    asyncio.run(create_new_csv(filename='met.csv', rows=csv_rows[:26], desired_data=desired_met_data))
     end = time.time()
-    print(f'searched {len(csv_rows)} links in {end - start} seconds')
+    print(f'searched {len(csv_rows[:26])} links in {end - start} seconds')
