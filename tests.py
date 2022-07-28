a668ac10107d4492a57cf4fcdccbec2f01049928
Campbell Padgett <campbellpadgett5@gmail.com>
2022-07-28 08:16:48 -0500

    tests complete

--- tests.py
+++ tests.py
@@ -1,42 +1,11 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# create new csvs
-# processors
-# csv traversers
-
-
-# RP-P-1997-357, http: // hdl.handle.net/10934/RM0001.COLLECT.356891, Affiche voor 'Kleine schilderijen' door Georg Rueter, affiche, "Rueter, Georg", 1945,
-# RP-P-2008-277, http: // hdl.handle.net/10934/RM0001.collect.465622, "Affiche voor expositie schilderijen en litho's van Klaas Gubbels in het Besiendershuis, Nijmegen", affiche, "Gubbels, Klaas", 1950,
-# RP-P-2015-26-1036,http://hdl.handle.net/10934/RM0001.COLLECT.613311,Verzamelaar van schilderijen,prent,"Chodowiecki, Daniel Nikolaus",1781,
-
-
-
-from re import S
 import unittest
 import asynctest
 import aiohttp
 import certifi
 import ssl
 import json
-from rjk import rjk
 
-from utils import isNotLatinAlphabet, filter_for_rjk_fields, chi_url_generator, csv_traverse, rjk_translated_csv_traverse
+import utils
 import settings
 
 class TestCSV_Traversers(unittest.TestCase):
@@ -46,22 +15,22 @@ class TestCSV_Traversers(unittest.TestCase):
         self.met = 'test_material/test_met.csv'
 
     def test_is_public_domain(self):
-        rjk_data = csv_traverse(self.rjk, ['schilderij'], 'RJK')
+        rjk_data = utils.csv_traverse(self.rjk, ['schilderij'], 'RJK')
         self.assertTrue(len(rjk_data) == 1)
         self.assertTrue(rjk_data[0][0] == 'AK-BR-325')
 
-        met_data = csv_traverse(self.met, ['Paintings'], 'MET')
+        met_data = utils.csv_traverse(self.met, ['Paintings'], 'MET')
         self.assertTrue(len(met_data) == 1)
         self.assertTrue(met_data[0][0] == '67.265.9')
 
     def test_title(self):
-        rjk_data = csv_traverse(self.rjk, ['schilderij'], 'RJK')
+        rjk_data = utils.csv_traverse(self.rjk, ['schilderij'], 'RJK')
         self.assertTrue(rjk_data[0][2] == 'Eivormige vaas met een rood glazuur')
 
-        met_data = csv_traverse(self.met, ['Paintings'], 'MET')
+        met_data = utils.csv_traverse(self.met, ['Paintings'], 'MET')
         self.assertTrue(met_data[0][9] == 'Two-and-a-Half Dollar Coin')
 
-        rjkT_data = rjk_translated_csv_traverse(self.rjk_t)
+        rjkT_data = utils.rjk_translated_csv_traverse(self.rjk_t)
         self.assertTrue(len(rjkT_data) == 1)
         self.assertTrue(rjkT_data[0][0] == 'De doop van de kamerling')
 
@@ -77,26 +46,24 @@ class TestCHI_URL_Generators(unittest.TestCase):
             a.close(), b.close()
 
     def test_accessable(self):
-        data_pass = chi_url_generator(self.json_1)
+        data_pass = utils.chi_url_generator(self.json_1)
         self.assertTrue(data_pass[0] == "Entombment of Christ")
         self.assertTrue(data_pass[-1] == 'https://www.artic.edu/iiif/2/ca6f5f33-9419-e60f-952f-fa642676eb61/full/843,/0/default.jpg')
         self.assertTrue(data_pass[-2] == 'https://www.artic.edu/iiif/2/ca6f5f33-9419-e60f-952f-fa642676eb61/full/1686,/0/default.jpg')
         self.assertTrue(data_pass[1] == "Unknown Artist Italian, Lombard, late 16th centlury")
         self.assertFalse(len(data_pass) >= 10 or len(data_pass) <= 8)
 
-        data_fail = chi_url_generator(self.json_2)
+        data_fail = utils.chi_url_generator(self.json_2)
         self.assertTrue(data_fail is None)
         
         self.json_2['is_public_domain'], self.json_2['image_id'] = True, None
-        data_fail = chi_url_generator(self.json_2)
+        data_fail = utils.chi_url_generator(self.json_2)
         self.assertTrue(data_fail is None)
 
 
 
     def tearDown(self):
         del self.json_1, self.json_2
-
-
 
 
 class TestRJKFilters(asynctest.TestCase):
@@ -120,7 +87,7 @@ class TestRJKFilters(asynctest.TestCase):
                 
 
     async def test_filter_for_rjk_fields1(self):
-        filtered_data1 = filter_for_rjk_fields(self.res1, self.image_link)
+        filtered_data1 = utils.filter_for_rjk_fields(self.res1, self.image_link)
 
         self.assertTrue(filtered_data1[0] == "Affiche voor 'Kleine schilderijen' door Georg Rueter")
         self.assertTrue(filtered_data1[9] == "1945 - 1960")
@@ -129,7 +96,7 @@ class TestRJKFilters(asynctest.TestCase):
             
 
     async def test_filter_for_rjk_fields2(self):
-        filtered_data2 = filter_for_rjk_fields(self.res2, self.image_link)
+        filtered_data2 = utils.filter_for_rjk_fields(self.res2, self.image_link)
 
         self.assertTrue(filtered_data2[0] == "Affiche voor expositie schilderijen en litho's van Klaas Gubbels in het Besiendershuis, Nijmegen")
         self.assertTrue(filtered_data2[9] == "ca. 1950 - ca. 1970")
@@ -138,7 +105,7 @@ class TestRJKFilters(asynctest.TestCase):
             
 
     async def test_filter_for_rjk_fields3(self):
-        filtered_data3 = filter_for_rjk_fields(self.res3, self.image_link)
+        filtered_data3 = utils.filter_for_rjk_fields(self.res3, self.image_link)
 
         self.assertTrue(filtered_data3[0] == "Verzamelaar van schilderijen")
         self.assertTrue(filtered_data3[9] == "1781")
@@ -147,7 +114,7 @@ class TestRJKFilters(asynctest.TestCase):
 
     def test_filter_for_rjk_fields_3(self):
         self.res3['artObject']['title'] = ''
-        filtered_data3 = filter_for_rjk_fields(self.res3, self.image_link)
+        filtered_data3 = utils.filter_for_rjk_fields(self.res3, self.image_link)
         self.assertTrue(filtered_data3 is None)
 
     def tearDown(self):
@@ -162,11 +129,11 @@ class TestIsNotLatinAlphabet(unittest.TestCase):
 
     def test_isNotLatinAlphabet_false(self):
         for term in self.false_terms:
-            self.assertFalse(isNotLatinAlphabet(term))
+            self.assertFalse(utils.isNotLatinAlphabet(term))
 
     def test_isNotLatinAlphabet_true(self):
         for term in self.true_terms:
-            self.assertTrue(isNotLatinAlphabet(term))
+            self.assertTrue(utils.isNotLatinAlphabet(term))
 
     def tearDown(self):
         del self.false_terms, self.true_terms
